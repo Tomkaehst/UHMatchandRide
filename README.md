0d635134f7744ee2e5bc381c29776b64d588b929
Tom Kache <tomkache@Toms-MacBook-Pro.local>
2022-09-25 10:09:14 +0200

    tutorial stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # UHMatchandRide
+
+Following tutorial: https://blog.yezz.me/blog/Build-and-Secure-an-API-in-Python-with-FastAPI
+
+See here for other resources and tutorials: https://github.com/mjhea0/awesome-fastapi#tutorials
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+from models import models
+from database.configuration import engine
+from core import blog, user, auth
+
+models.Base.metadata.create_all(bind = engine)
+
+app = FastAPI(
+    title = "SecureAPI",
+    description = "test",
+    version = "1.0.0"
+)
+
+app.include_router(blog.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+
+@app.get("/", response_class = HTMLResponse)
+def index():
+    return """
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1>SecureAPI</h1>
+            <div class="btn-group">
+                <a href="/docs"><button>SwaggerUI</button></a>
+                <a href="/redoc"><button>Redoc</button></a>
+            </div>
+        </body> 
+    </html
+    """
